e6f33f770549ff859085f1ac77e008331dfd8059
gudszent <gudszent@gmail.com>
2020-09-24 21:47:20 +0200

    .

--- !Edited/tmp/deploy.ps1
+++ !Edited/tmp/deploy.ps1
@@ -1,4 +1,4 @@
-$rg='test3-deploy'
+$rg='test-deploy'
 New-AzResourceGroup -Name $rg -Location 'North Europe' -Force
 
 New-AzResourceGroupDeployment -Name 'adalwayson-gen2' -ResourceGroupName $rg -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json
