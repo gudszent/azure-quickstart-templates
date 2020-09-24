ca302c7bcbc0b9a7f351ed0487a68724ae584b35
gudszent <gudszent@gmail.com>
2020-09-24 17:06:26 +0200

    .

--- !Edited/tmp/deploy.ps1
+++ !Edited/tmp/deploy.ps1
@@ -1,4 +1,4 @@
-$rg='test2-deploy'
+$rg='test3-deploy'
 New-AzResourceGroup -Name $rg -Location 'North Europe' -Force
 
 New-AzResourceGroupDeployment -Name 'adalwayson-gen2' -ResourceGroupName $rg -TemplateFile D:\GitHub\azure-quickstart-templates\!Edited\tmp\azuredeploy.json -TemplateParameterFile D:\GitHub\azure-quickstart-templates\!Edited\tmp\azuredeploy.parameters.json
