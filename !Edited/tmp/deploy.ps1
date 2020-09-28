188288fb752a56b3a9004903f80b6a796f7c02bf
gudszent <gudszent@gmail.com>
2020-09-28 22:32:40 +0200

    .

--- !Edited/tmp/deploy.ps1
+++ !Edited/tmp/deploy.ps1
@@ -1,4 +1,4 @@
-$rg='test-deploy'
+$rg='test2-deploy'
 New-AzResourceGroup -Name $rg -Location 'North Europe' -Force
 
 New-AzResourceGroupDeployment -Name 'deployka' -ResourceGroupName $rg -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json
